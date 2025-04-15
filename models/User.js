d9ff9b561fccd929472561913aefcccb02149759
MH-Hassan02 <haseebhassan1155@gmail.com>
2025-04-15 14:32:09 +0500

    Password

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: false },
     googleId: { type: String, unique: true },
     profilePic: {
       type: String,
